a8d4326213c76d2e947a413a1fc894cedf9e126e
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-03-17 00:13:37 +0300

    Add GetStroke method that extracts strokes from an image [ImageTools.cs]

--- PaintingsGenerator/ImageTools.cs
+++ PaintingsGenerator/ImageTools.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 
+using PaintingsGenerator.Colors;
 using PaintingsGenerator.Images;
+using PaintingsGenerator.MathStuff;
 using PaintingsGenerator.Images.ImageStuff;
 
 namespace PaintingsGenerator {
@@ -25,6 +27,41 @@ namespace PaintingsGenerator {
             }
 
             return posWithMaxDiff;
+        }
+
+        public static StrokePositions GetStroke(RGBImage image, Gradient gradient,
+                                                Position start, uint height,
+                                                int maxError = 4) {
+            var positions = new StrokePositions { start };
+            var prevColor = image.GetColor(positions[^1], height);
+            var prevVec = gradient.GetPerpVector(positions[^1]);
+            var step = height * 2 + 1;      // Step = width of brush
+
+            while (true) {
+                prevVec = gradient.GetPerpVector(positions[^1], prevVec);
+                prevVec.Normalize();
+                if (prevVec.IsPoint()) break;
+
+                var newPos = GetNewPosition(positions[^1], prevVec, step);
+                if (!newPos.InBounds(0, 0, image.Width - 1, image.Height - 1)) break;
+
+                var newColor = image.GetColor(newPos, height);
+                var curError = RGBColor.Difference(prevColor, newColor);
+                if (curError > maxError) break;
+
+                positions.Add(newPos);
+                prevColor = newColor;
+            }
+
+            return positions;
+        }
+
+        private static Position GetNewPosition(Position pos, Vector2D direction,
+                                               uint step) {
+            var newX = pos.X + direction.X*step;
+            var newY = pos.Y + direction.Y*step;
+
+            return new((int)newX, (int)newY);
         }
     }
 }
