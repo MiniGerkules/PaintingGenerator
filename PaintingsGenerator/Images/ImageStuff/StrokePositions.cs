695f17441a89bf066f55f8847657401f1c9e1627
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-03-16 23:48:34 +0300

    Add an indexer and Count property to StrokePositions class [Images/ImageStuff/StrokePositions.cs]

--- PaintingsGenerator/Images/ImageStuff/StrokePositions.cs
+++ PaintingsGenerator/Images/ImageStuff/StrokePositions.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 namespace PaintingsGenerator.Images.ImageStuff {
     public class StrokePositions : IEnumerable<Position>{
         private readonly List<Position> positions = new();
+        public Position this[int i] => positions[i];
+        public int Count => positions.Count;
 
         public void Add(Position position) => positions.Add(position);
 
