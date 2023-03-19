9e9122a4f9d080e84d2346d968872f199102e66a
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-03-20 01:42:46 +0300

    Change formatting [Images/ImageStuff/StrokePositions.cs]

--- PaintingsGenerator/Images/ImageStuff/StrokePositions.cs
+++ PaintingsGenerator/Images/ImageStuff/StrokePositions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 
 namespace PaintingsGenerator.Images.ImageStuff {
-    public class StrokePositions : IEnumerable<Position>{
+    public class StrokePositions : IEnumerable<Position> {
         private readonly List<Position> positions = new();
         public Position this[int i] => positions[i];
         public int Count => positions.Count;
