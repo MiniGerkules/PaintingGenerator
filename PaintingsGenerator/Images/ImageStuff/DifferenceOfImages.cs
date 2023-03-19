b514049a2104f170df2544915c3934e22392462e
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-03-20 01:34:12 +0300

    Add DifferenceOfImages class that contains difference of two images [Images/ImageStuff/DifferenceOfImages.cs]

---
+++ PaintingsGenerator/Images/ImageStuff/DifferenceOfImages.cs
@@ -0,0 +1,29 @@
+﻿namespace PaintingsGenerator.Images.ImageStuff {
+    internal class DifferenceOfImages : Container<ushort> {
+        public DifferenceOfImages(int width, int height) : base(new ushort[height, width]) {
+        }
+
+        public double SumDiff() {
+            ulong diffSum = 0;
+
+            for (int y = 0; y < Height; ++y) {
+                for (int x = 0; x < Width; ++x)
+                    diffSum += this[y, x];
+            }
+
+            return (double)diffSum / (Width*Height);
+        }
+
+        public double GetDifference(Position pos, uint height) {
+            var part = GetPart(pos, height);
+
+            ulong diff = 0;
+            for (int y = 0; y < part.Height; ++y) {
+                for (int x = 0; x < part.Width; ++x)
+                    diff += part[y, x];
+            }
+
+            return (double)diff / part.Size;
+        }
+    }
+}
