179a4eafb2f10742084859b7e7ffff0780642233
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-04-17 01:44:04 +0300

    Change Position to StrokePivot [Images/ImageStuff/StrokePositions.cs]

--- PaintingsGenerator/Images/ImageStuff/StrokePositions.cs
+++ PaintingsGenerator/Images/ImageStuff/StrokePositions.cs
@@ -1,21 +1,20 @@
-﻿using System.Linq;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 
 namespace PaintingsGenerator.Images.ImageStuff {
-    public class StrokePositions : IEnumerable<Position> {
-        private List<Position> positions = new();
-        public Position this[int i] => positions[i];
+    public class StrokePositions : IEnumerable<StrokePivot> {
+        private List<StrokePivot> positions = new();
+        public StrokePivot this[int i] => positions[i];
         public int Count => positions.Count;
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-        public IEnumerator<Position> GetEnumerator() {
+        public IEnumerator<StrokePivot> GetEnumerator() {
             foreach (var pos in positions)
                 yield return pos;
         }
 
-        public void Add(Position position) => positions.Add(position);
+        public void Add(StrokePivot position) => positions.Add(position);
         public void Add(StrokePositions strokePositions) => positions.AddRange(strokePositions);
 
         public void MakeUnique() {
