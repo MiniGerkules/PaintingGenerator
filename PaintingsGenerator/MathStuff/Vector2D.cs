49b48b33638383da80c91b77cdb986d729c5cef2
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-04-21 00:48:50 +0300

    Add new constructor and Length property for Vector2D [MathStuff/Vector2D.cs]

--- PaintingsGenerator/MathStuff/Vector2D.cs
+++ PaintingsGenerator/MathStuff/Vector2D.cs
@@ -4,10 +4,15 @@ namespace PaintingsGenerator.MathStuff {
     internal struct Vector2D {
         public double X { get; private set; }
         public double Y { get; private set; }
+        public double Length { get; }
 
         public Vector2D(double x, double y) {
             X = x;
             Y = y;
+            Length = Math.Sqrt(X*X + Y*Y);
+        }
+
+        public Vector2D(Position start, Position end) : this(end.X - start.X, end.Y - start.Y) {
         }
 
         public Vector2D Reverse() => new(-X, -Y);
