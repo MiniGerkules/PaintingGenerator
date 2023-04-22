7e699bdc2054e78a706876dcbc9c6966c4c83ea9
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-04-22 19:21:14 +0300

    Add a new method that returns stroke bounds [Images/ImageStuff/Stroke.cs]

--- PaintingsGenerator/Images/ImageStuff/Stroke.cs
+++ PaintingsGenerator/Images/ImageStuff/Stroke.cs
@@ -1,4 +1,6 @@
-﻿namespace PaintingsGenerator.Images.ImageStuff {
+﻿using System;
+
+namespace PaintingsGenerator.Images.ImageStuff {
     public class Stroke<ColorType> {
         public StrokePositions Positions { get; }
         public ColorType Color { get; }
@@ -6,6 +8,20 @@
         public Stroke(StrokePositions positions, ColorType color) {
             Positions = positions;
             Color = color;
+        }
+
+        public Bounds GetStrokeBounds() {
+            int leftBound = int.MaxValue, rightBound = int.MinValue;
+            int upBound = int.MinValue, downBound = int.MaxValue;
+
+            foreach (var pos in Positions) {
+                leftBound = Math.Min(leftBound, pos.Position.X - (int)pos.Radius);
+                rightBound = Math.Max(rightBound, pos.Position.X + (int)pos.Radius);
+                upBound = Math.Max(upBound, pos.Position.Y + (int)pos.Radius);
+                downBound = Math.Min(downBound, pos.Position.Y - (int)pos.Radius);
+            }
+
+            return new(leftBound, rightBound, upBound, downBound);
         }
     }
 }
