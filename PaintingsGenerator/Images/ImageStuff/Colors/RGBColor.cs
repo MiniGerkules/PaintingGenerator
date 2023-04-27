9071b755f21dbc28da43398a0b011ceddf4c1c70
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-04-27 18:27:03 +0300

    Make RGBColor a positional record [Images/ImageStuff/Colors/RGBColor.cs]

--- PaintingsGenerator/Images/ImageStuff/Colors/RGBColor.cs
+++ PaintingsGenerator/Images/ImageStuff/Colors/RGBColor.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 
 namespace PaintingsGenerator.Images.ImageStuff.Colors {
-    public record struct RGBColor {
-        public byte Red { get; }
-        public byte Green { get; }
-        public byte Blue { get; }
-
-        public RGBColor(byte red, byte green, byte blue) {
-            Red = red;
-            Green = green;
-            Blue = blue;
-        }
-
+    public record struct RGBColor(byte Red, byte Green, byte Blue) {
         public static uint Difference(RGBColor a, RGBColor b) {
             return (uint)Math.Abs(a.Red - b.Red) +
                    (uint)Math.Abs(a.Green - b.Green) +
