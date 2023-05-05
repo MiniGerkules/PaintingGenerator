4714dd9b42000b4da0aaba01adeb3ce85248a1e4
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-05-06 00:47:48 +0300

    Add a class to add a method that checks a BitmapSource to null [NotifierWithImageProp.cs]

---
+++ PaintingsGenerator/NotifierWithImageProp.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Media.Imaging;
+using System.Runtime.CompilerServices;
+
+namespace PaintingsGenerator {
+    public class NotifierWithImageProp : NotifierOfPropertyChange {
+        protected void NotifyPropertyChangedIfNeed([CallerMemberName] string propertyName = "",
+                                                   BitmapSource? bitmap = null) {
+            if (bitmap != null)
+                NotifyPropertyChanged(propertyName);
+        }
+    }
+}
