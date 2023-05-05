d68402767441ce52e8542de3be66ccaa243d3622
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-05-05 23:08:07 +0300

    Make RGBColor and GrayColor implements IStrokeColor interface [Images/ImageStuff/GrayColor.cs, Images/ImageStuff/RGBColor.cs]

--- PaintingsGenerator/Images/ImageStuff/Colors/RGBColor.cs
+++ PaintingsGenerator/Images/ImageStuff/Colors/RGBColor.cs
@@ -1,7 +1,18 @@
 ﻿using System;
+using System.Windows.Media;
+
+using PaintingsGenerator.StrokesLib.Colors;
 
 namespace PaintingsGenerator.Images.ImageStuff.Colors {
-    public record struct RGBColor(byte Red, byte Green, byte Blue) {
+    public record struct RGBColor(byte Red, byte Green, byte Blue) : IStrokeColor {
+        public RGBColor(Color color) : this(color.R, color.G, color.B) {
+        }
+
+        public bool IsTransparent => false;
+
+        public bool IsEqual(Color color) => Red == color.R && Green == color.G && Blue == color.B;
+        public Color ToColor() => Color.FromRgb(Red, Green, Blue);
+
         public static uint Difference(RGBColor a, RGBColor b) {
             return (uint)Math.Abs(a.Red - b.Red) +
                    (uint)Math.Abs(a.Green - b.Green) +
