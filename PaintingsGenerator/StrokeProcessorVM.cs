40ad488fdc56469a634c7be8eefe6c28debdec7e
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-05-06 00:48:31 +0300

    Add a view model for displaying strokes [StrokeProcessorVM.cs]

---
+++ PaintingsGenerator/StrokeProcessorVM.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace PaintingsGenerator {
+    public class StrokeProcessorVM : NotifierWithImageProp {
+        private BitmapSource? generatedStroke;
+        public BitmapSource? GeneratedStroke {
+            get => generatedStroke;
+            set {
+                generatedStroke = value;
+                NotifyPropertyChangedIfNeed(nameof(GeneratedStroke), value);
+            }
+        }
+
+        private BitmapSource? libStroke;
+        public BitmapSource? LibStroke {
+            get => libStroke;
+            set {
+                libStroke = value;
+                NotifyPropertyChangedIfNeed(nameof(LibStroke), value);
+            }
+        }
+    }
+}
