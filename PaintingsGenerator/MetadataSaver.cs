64422464ce55026f41ac741eecb1e1b106e03d78
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-05-09 21:38:19 +0300

    Add class that makes json with metadata [MetadataSaver.cs]

---
+++ PaintingsGenerator/MetadataSaver.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Text.Json;
+
+using PaintingsGenerator.Images.ImageStuff;
+
+namespace PaintingsGenerator {
+    public class MetadataSaver {
+        private StreamWriter? metadata;
+        private bool haveARecord = false;
+
+        public void StartWrite(string pathToMetadata) {
+            if (metadata != null) metadata.Close();
+
+            haveARecord = false;
+            metadata = new(pathToMetadata + "_metadata.json", false);
+            metadata.Write("{\"parameters\": [");
+        }
+
+        public void Write(StrokeParameters parameters) {
+            if (metadata == null) return;
+            
+            if (haveARecord) metadata.Write(',');
+            var paramInJson = JsonSerializer.Serialize(parameters);
+            metadata.Write(paramInJson);
+            
+            haveARecord = true;
+        }
+
+        public void EndWrite() {
+            if (metadata != null) {
+                metadata.Write("]}");
+                metadata.Close();
+            }
+        }
+    }
+}
